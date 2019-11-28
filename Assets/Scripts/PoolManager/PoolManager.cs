3d07eeb66654d55e830914a36ce7f6e8f0450abb
wuxiaos <626429868@qq.com>
2019-11-28 23:21:07 +0800

    1

--- Assets/Scripts/PoolManager/PoolManager.cs
+++ Assets/Scripts/PoolManager/PoolManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PoolManager {
     // Start is called before the first frame update
     private Dictionary<PoolType, Pool> _dict;
+    PoolManager() {
+        
+    }
     public GameObject getGameObject() {
         return new GameObject();
     }
